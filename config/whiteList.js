1a23964df5a9652b120784a2b5fd6ea7a84b2b4d
masangakevin256 <masangakevin1@gmail.com>
2025-10-05 17:33:53 +0300

    whitelist

--- config/whiteList.js
+++ config/whiteList.js
@@ -1,7 +1,7 @@
 const whiteLit = [
     "http://127.0.0.1:5500/",
-    "www.google.com",
-    "localhost:3500"
+    "https://www.google.com/",
+    "http://localhost:3500/"
 ]
 
 module.exports = whiteLit;
